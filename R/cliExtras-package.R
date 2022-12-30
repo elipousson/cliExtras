ffb47d40a4f2d74b8824f05148c0728c044bd1ef
Eli Pousson <eli.pousson@gmail.com>
2022-12-30 00:17:17 -0800

    refactor: adjust cli_abort_ifnot to be more similar to stopifnot
    - refactor: adjust cli_abort_ifnot to be more similar to stopifnot + remove .data parameter
    - feat: add cli_list_files + cli_menu

--- R/cliExtras-package.R
+++ R/cliExtras-package.R
@@ -2,7 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom rlang caller_env
+#' @importFrom rlang caller_env `%||%`
 #' @importFrom cli cli_abort cli_warn cli_inform
 ## usethis namespace: end
 NULL
--- R/cliExtras.R
+++ R/cliExtras.R
@@ -35,131 +35,189 @@ cli_yesno <- function(message,
 
 #' Display a Message then Read a Line from the Terminal
 #'
-#' @param message Primary prompt to user prefixed by ">" when passed to
-#'   [cli_inform()].
-#' @param ... Secondary messages passed to [cli_inform()]
-#' @param prompt Character to show as user prompt in console following displayed
-#'   message.
+#' @param message Primary prompt to user passed to [cli_inform()].
+#' @param prompt Characters to show as user prompt in console following
+#'   displayed message.
 #' @inheritParams cli::cli_inform
 #' @export
 cli_ask <- function(message, ..., prompt = ">> ", .envir = parent.frame()) {
-  cli_inform(c(">" = message, ...), .envir = .envir)
+  cli::cli_inform(message, ..., .envir = .envir)
   readline(prompt = prompt)
 }
 
-#' Signal an error, warning or message with a cli formatted message if a
-#' condition is FALSE
+#' Display a Message then Read a Line from the Terminal
 #'
+#' @param title title for menu
 #'
-#' @param condition if condition is not logical, it is assumed to be a single
-#'   argument function that is applied to the object passed to the .data
-#'   parameter.
-#' @param .data object to test with condition if condition is a function.
-#' @inheritParams cli::cli_abort
+#' @param prompt Characters to show as user prompt in console following
+#'   displayed menu options.
+#' @param ... Secondary messages passed to [cli_inform()] with title as message.
+#' @inheritParams cli_ask
+#' @inheritParams cli::cli_inform
 #' @export
-#' @importFrom rlang is_logical as_function
-cli_abort_ifnot <- function(...,
-                            condition = FALSE,
-                            .data = NULL,
-                            call = .envir,
-                            .envir = parent.frame()) {
-  if (!rlang::is_logical(condition)) {
-    condition <- condition_as_fn(condition, .data, call = call)
-  }
+cli_menu <- function(choices,
+                     title = NULL,
+                     message = NULL,
+                     prompt = ">> ",
+                     choice_style = c("num", "val"),
+                     ...) {
+  rlang::check_required(choices)
 
-  if (!condition) {
-    cli_abort(
-      ...,
-      call = call,
-      .envir = .envir
+  if (!is.null(title)) {
+    cli::cli_inform(
+      message = title,
+      ...
     )
   }
 
-  invisible(NULL)
+  if (!rlang::is_named(choices)) {
+    choices <- rlang::set_names(choices, as.character(c(1:length(choices))))
+  }
+
+  cli_ul_items(
+    choices,
+    style = choice_style
+  )
+
+  choices[[cli_ask(message = message %||% "Selection:", prompt = prompt)]]
+}
+
+#' Signal an error, warning, or message with a cli formatted message if any
+#' expressions in ... are not all TRUE
+#'
+#' @param ... Any number of R expressions which should each evaluate to a
+#'   logical vector. If ... is named, the names will be passed to as the message
+#'   parameter. If message is provided, any names for the logical expressions
+#'   are ignored.
+#' @param condition Deprecated. Logical. If `FALSE`, signal an error, warning,
+#'   or message. Defaults to `NULL`.
+#' @inheritParams cli::cli_abort
+#' @export
+#' @importFrom rlang list2 is_true is_named
+#' @importFrom cli cli_abort
+cli_abort_ifnot <- function(...,
+                            message = NULL,
+                            call = .envir,
+                            .envir = parent.frame(),
+                            .frame = .envir,
+                            condition = NULL) {
+  cli_ifnot(
+    ...,
+    message = message,
+    call = call,
+    .envir = .envir,
+    .frame = .frame,
+    condition = condition,
+    fn = cli::cli_abort
+  )
 }
 
 #' @name cli_warn_ifnot
 #' @rdname cli_abort_ifnot
-#' @export
-#' @importFrom rlang is_logical
 cli_warn_ifnot <- function(...,
-                           condition = FALSE,
-                           .data = NULL,
-                           .envir = parent.frame()) {
-  if (!rlang::is_logical(condition)) {
-    condition <- condition_as_fn(condition, .data, call = call)
-  }
-
-  if (!condition) {
-    cli_warn(
-      ...,
-      .envir = .envir
-    )
-  }
-
-  invisible(NULL)
-}
-
-
-#' @name cli_inform_ifnot
-#' @rdname cli_abort_ifnot
-#' @export
-#' @importFrom rlang is_logical
-cli_inform_ifnot <- function(...,
-                             condition = FALSE,
-                             .data = NULL,
-                             .envir = parent.frame()) {
-  if (!rlang::is_logical(condition)) {
-    condition <- condition_as_fn(condition, .data, call = call)
-  }
-
-  if (!condition) {
-    cli_inform(
-      ...,
-      .envir = .envir
-    )
-  }
-
-  invisible(NULL)
-}
-
-
-#' Use condition argument as function
-#'
-#' @noRd
-#' @importFrom rlang is_function as_function is_logical
-condition_as_fn <- function(condition,
-                            .data = NULL,
-                            call = parent.frame()) {
-  if (!rlang::is_function(condition)) {
-    condition <- rlang::as_function(condition)
-  }
-
-  condition <- condition(.data)
-
-  if (rlang::is_logical(condition)) {
-    return(condition)
-  }
-
-  cli_abort(
-    "{.fn condition} returned a {.cls {class(condition)} object, not logical.",
-    call = call
+                           message = NULL,
+                           .envir = parent.frame(),
+                           condition = NULL) {
+  cli_ifnot(
+    ...,
+    message = message,
+    .envir = .envir,
+    condition = condition,
+    fn = cli::cli_warn
   )
 }
 
+#' @name cli_inform_ifnot
+#' @rdname cli_abort_ifnot
+cli_inform_ifnot <- function(...,
+                             message = NULL,
+                             .envir = parent.frame(),
+                             condition = NULL) {
+  cli_ifnot(
+    ...,
+    message = message,
+    .envir = .envir,
+    condition = condition,
+    fn = cli::cli_inform
+  )
+}
+
+#' @noRd
+#' @importFrom rlang list2 is_true is_named
+cli_ifnot <- function(...,
+                      message = NULL,
+                      call = NULL,
+                      .envir = parent.frame(),
+                      .frame = NULL,
+                      condition = NULL,
+                      fn = NULL) {
+  params <- rlang::list2(...)
+
+  if (length(params) > 1) {
+    for (x in seq(params)) {
+      cli_ifnot(
+        params[[x]],
+        message = message %||% names(params)[x],
+        .envir = .envir,
+        fn = fn
+      )
+    }
+
+    return(invisible(NULL))
+  }
+
+  # NOTE: This is to support backwards compatibility with the prior syntax
+  if (!is.null(condition) && !condition) {
+    message <- params[[1]]
+    params[[1]] <- FALSE
+  }
+
+  if (!rlang::is_true(params[[1]])) {
+    if (rlang::is_named(params)) {
+      message <- message %||% names(params)[1]
+    }
+
+    if (!is.null(call)) {
+      fn(
+        message = message,
+        call = call,
+        .envir = .envir,
+        .frame = .frame
+      )
+    } else {
+      fn(
+        message = message,
+        .envir = .envir
+      )
+    }
+  }
+
+  invisible(NULL)
+}
 
 #' Format a list of items as an unordered list with cli_ul()
 #'
 #' @rdname cli_ul_items
+#' @inheritParams cli::cli_li
+#' @param style Length 2 character vector indicating cli style to use for names
+#'   of items and style to use for items.
+#' @param sep Length 1 character vector with separator between item names and
+#'   names.
 #' @export
 #' @importFrom cli cli_ul cli_li cli_end
-cli_ul_items <- function(items) {
+cli_ul_items <- function(items, style = c("code", "val"), sep = ": ") {
   items <- sapply(items, as.character)
   cli::cli_ul()
   sapply(
     seq_along(items),
     function(x) {
-      cli::cli_li("{.code {names(items)[[x]]}}: {.val {items[[x]]}}")
+      cli::cli_li(
+        paste0(
+          "{.", style[1], " {names(items)[[x]]}}",
+          sep,
+          "{.", style[2], " {items[[x]]}}"
+        )
+      )
     }
   )
   cli::cli_end()
@@ -189,4 +247,37 @@ cli_paths <- function(path,
 
   path <- paste0("{.file ", path, "}")
   cli::cli_bullets(rlang::set_names(path, rep("*", len_path)))
+}
+
+#' Display a list of files as a list of items
+#'
+#' @inheritParams base::list.files
+#' @param message Passed to [cli::cli_inform()].
+#' @param .envir Passed to [cli::cli_inform()]. Defaults to
+#'   [rlang::current_env()] rather than [parent.frame()] to support evaluation
+#'   of default message that includes the number of files found at the path.
+#' @inheritDotParams base::list.files
+#' @seealso
+#'  [cli::cli_bullets()]
+#' @name cli_list_files
+#' @export
+#' @importFrom cli cli_inform cli_bullets
+#' @importFrom rlang set_names
+cli_list_files <- function(path,
+                           pattern = NULL,
+                           full.names = TRUE,
+                           message = "{length(files)} file{?s} found in {.path {path}}:",
+                           bullet = "*",
+                           .envir = current_env(),
+                           ...) {
+  files <- list.files(path = path, pattern = pattern, full.names = full.names, ...)
+
+  cli::cli_inform(
+    message = message,
+    .envir = .envir
+  )
+
+  cli::cli_bullets(
+    rlang::set_names(paste0("{.file ", files, "}"), rep(bullet, length(files)))
+  )
 }
