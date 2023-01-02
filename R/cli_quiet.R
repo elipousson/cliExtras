a1ee97067ba2f390a15f9fe4efd3cb5928f24c86
Eli Pousson <eli.pousson@gmail.com>
2023-01-02 00:02:14 -0800

    refactor: switch cli_quiet to use the cli.default_handler option
    refactor: switch cli_quiet to use the cli.default_handler option + switch cliExtras.quiet to work w/ quiet_cli_inform (dropping quiet_cli_alert + quiet_cli_bullets

--- R/cli_quiet.R
+++ R/cli_quiet.R
@@ -1,28 +1,59 @@
-#' Helper functions to get or set the cli.quiet option
+#' Helper functions to get or set the cliExtras.quiet option
 #'
-#' [cli_quiet()] gets the cli.quiet option and [set_cli_quiet()] sets the
+#' [cli_quiet()] gets the cliExtras.quiet option and [set_cli_quiet()] sets the
 #' option.
 #' @param quiet Option value to set or default value if option is not set.
 #'   Defaults to `FALSE`.
 #' @export
 cli_quiet <- function(quiet = FALSE) {
-  getOption("cli.quiet", default = quiet)
+  getOption("cliExtras.quiet", default = quiet)
 }
 
 #' @name set_cli_quiet
 #' @rdname cli_quiet
 #' @export
+#' @importFrom cli cli_rule cli_inform cli_end
 set_cli_quiet <- function(quiet = FALSE) {
-  options("cli.quiet" = quiet)
+  handler_label <- "`NULL`"
+  quiet_label <- "allow"
+  default_handler <- NULL
+
+  if (quiet) {
+    quiet_label <- "muffle"
+    handler_label <- cli::cli_fmt(cli::cli_text("{.fn suppressMessages}"))
+    default_handler <- suppressMessages
+  }
+
+  if (cli_quiet() && !quiet) {
+    options("cli.default_handler" = default_handler)
+    options("cliExtras.quiet" = quiet)
+  }
+
+  cli::cli_rule("Updating options", id = "set.cli.quiet")
+  cli::cli_inform(
+    c(
+      "v" = "Setting {.field cli.default_handler} to {handler_label} to
+      {quiet_label} {.pkg cli} messages.",
+      "v" = "Setting {.field cliExtras.quiet} to {.code {quiet}} to
+      {quiet_label} warnings and info messages for {.pkg cliExtras}."
+    ),
+    id = "set.cli.quiet"
+  )
+  cli::cli_end(id = "set.cli.quiet")
+
+  options("cli.default_handler" = default_handler)
+  options("cliExtras.quiet" = quiet)
 }
 
-#' Quiet versions of [cli_inform()], [cli_bullets()], and [cli_alert()]
+#' Quiet version of [cli_inform()]
 #'
-#' The implementation of the cli.quiet option is based on the implementation of
-#' a similar setting in googlesheets4, googledrive, and gargle.
+#' The implementation of the cliExtras.quiet option is based on the
+#' implementation of a similar setting in googlesheets4, googledrive, and
+#' gargle. If the cliExtras.quiet option is set to `TRUE`, [quiet_cli_inform()]
+#' does not trigger a message. It is used by [cli_inform_ifnot()] and
+#' [cli_inform_if()] but not by most other functions in cliExtras.
 #'
-#' @param ... Additional parameters passed to [cli_inform()], [cli_bullets()],
-#'   or [cli_alert()]
+#' @param ... Additional parameters passed to [cli_inform()]
 #' @inheritParams cli::cli_inform
 #' @export
 #' @importFrom rlang is_true
@@ -34,40 +65,6 @@ quiet_cli_inform <- function(..., .envir = parent.frame()) {
   }
 
   cli::cli_inform(
-    ...,
-    .envir = .envir
-  )
-}
-
-#' @name quiet_cli_bullets
-#' @rdname quiet_cli_inform
-#' @export
-#' @importFrom rlang is_true
-#' @importFrom cli cli_bullets
-quiet_cli_bullets <- function(..., .envir = parent.frame()) {
-  quiet <- cli_quiet()
-  if (!is.na(quiet) && rlang::is_true(quiet)) {
-    return(invisible())
-  }
-
-  cli::cli_bullets(
-    ...,
-    .envir = .envir
-  )
-}
-
-#' @name quiet_cli_alert
-#' @rdname quiet_cli_inform
-#' @export
-#' @importFrom rlang is_true
-#' @importFrom cli cli_alert
-quiet_cli_alert <- function(..., .envir = parent.frame()) {
-  quiet <- cli_quiet()
-  if (!is.na(quiet) && rlang::is_true(quiet)) {
-    return(invisible())
-  }
-
-  cli::cli_alert(
     ...,
     .envir = .envir
   )
