4bf8fe315ee4e1b8f836b8560d11821b80084a2c
Eli Pousson <eli.pousson@gmail.com>
2023-01-03 14:29:29 -0800

    refactor(cli_ask): switch default prompt to '?'

--- R/cli_ask.R
+++ R/cli_ask.R
@@ -1,14 +1,17 @@
 #' Display a Message then Read a Line from the Terminal
 #'
-#' This function aborts if the session is not interactive.
-#'
 #' @param message Primary prompt to user passed to [cli::cli_inform()].
 #' @param prompt Characters to show as user prompt in console following
-#'   displayed message.
+#'   displayed message. A non-breaking space is always placed after the prompt
+#'   before passing to [readline()].
 #' @inheritParams cli::cli_inform
+#' @seealso [cli_yesno()]
 #' @export
 #' @importFrom cli cli_inform cat_rule
-cli_ask <- function(prompt = ">>", message = NULL, ..., .envir = parent.frame()) {
+cli_ask <- function(prompt = "?",
+                    message = NULL,
+                    ...,
+                    .envir = parent.frame()) {
   check_interactive()
   if (!is.null(message)) {
     cli::cli_text(message, ..., .envir = .envir)
