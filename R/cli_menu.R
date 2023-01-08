3a20863c456fdd2ca9655d6cf8e8cc11cba49098
Eli Pousson <eli.pousson@gmail.com>
2023-01-07 21:39:44 -0500

    refactor: simplify cli_menu to use numbered lists only

--- R/cli_menu.R
+++ R/cli_menu.R
@@ -3,16 +3,12 @@
 #' @param title Title for menu. Character vector passed to [cli::cli_h2()] if
 #'   title is length 1 or to [cli::cli_bullets()] if length is greater than 1.
 #' @param message Additional message to display after choices and before prompt.
-#' @param choices Required list of choices. If named, use the names as the
-#'   choice options.
+#' @param choices Required vector or list of choices.
 #' @param prompt Characters to show as user prompt in console following
 #'   displayed menu options. Defaults to "Selection:" (matching [utils::menu()])
-#' @param labels Style to use for menu choices (from
-#'   [isstatic::as_numbered_labels]). Defaults to "arabic". Ignored if choices
-#'   are a named vector or list.
-#' @param sep Separator between choice name and value. Ignored if choices
-#'   are a named vector or list.
 #' @param exit Character to use to exit menu.
+#' @param ind If `TRUE`, return index position, if `FALSE` (default), return
+#'   item from choices.
 #' @inheritParams cli_bulletize
 #' @inheritParams cli_ask
 #' @inheritParams cli::cli_inform
@@ -27,16 +23,13 @@
 #' }
 #' }
 #' @export
-#' @importFrom rlang check_required is_named set_names
-#' @importFrom cli cli_h2 cli_inform cat_rule console_width cli
+#' @importFrom rlang check_required set_names
+#' @importFrom cli cli_h2 cli_bullets cat_rule console_width cli_ol cli
 cli_menu <- function(choices,
                      title = NULL,
                      message = "Enter your selection or press {.kbd 0} to exit.",
                      prompt = "Selection:",
-                     labels = "arabic",
-                     sep = ": ",
-                     bullet = "",
-                     exit = "0",
+                     exit = 0,
                      ind = FALSE,
                      id = NULL,
                      call = .envir,
@@ -51,14 +44,11 @@ cli_menu <- function(choices,
     }
   }
 
-  title <- title %||% cli::cat_rule(width = cli::console_width() / 2)
-
-  if (!rlang::is_named(choices)) {
-    choices <- rlang::set_names(
-      choices,
-      as_numbered_labels(choices, labels)
+  title <- title %||% cli::cat_rule(
+    width = as.integer(cli::console_width() / 2)
     )
-  }
+
+  choices <- rlang::set_names(choices, seq_along(choices))
 
   choice_bullets <- NULL
 
@@ -69,20 +59,17 @@ cli_menu <- function(choices,
       choice_bullets[[i]] <-
         cli_bulletize(
           "{choices[i]}",
-          bullet = bullet,
+          bullet = "",
           before = names(choices[i]),
-          sep = sep,
+          sep = ". ",
           id = id,
           .envir = current_env()
         )
     }
   } else if (is.vector(choices)) {
     choice_bullets <-
-      cli_bulletize(
+      cli::cli_ol(
         choices,
-        before = names(choices),
-        bullet = bullet,
-        sep = sep,
         id = id
       )
   }
@@ -97,55 +84,28 @@ cli_menu <- function(choices,
     choice_bullets
   })
 
-  choose_from_menu(
-    message = message,
-    prompt = prompt,
-    choices = choices,
-    exit = exit,
-    ind = ind
-  )
-}
-
-#' @noRd
-#' @importFrom rlang has_name
-choose_from_menu <- function(prompt = ">>",
-                             message = NULL,
-                             choices,
-                             exit = "0",
-                             ind = FALSE,
-                             .envir = current_env(),
-                             ...) {
   choice <- cli_ask(
-    prompt = prompt,
+    prompt,
     message,
-    .envir = parent.frame(),
-    ...
+    .envir = .envir
   )
 
-  choice <- as.character(tolower(choice))
-
-  # FIXME: Switch from recursive calls to while structure
-  if (choice == tolower(exit)) {
-    return(invisible())
-  }
-
-  nm <- tolower(names(choices))
-
-  if (choice %in% nm) {
-    choice <- which(nm == choice)
-
-    if (ind) {
-      return(choice)
+  while(TRUE) {
+    if (choice == as.integer(exit)) {
+      return(invisible())
     }
 
-    return(choices[[choice]])
-  }
+    if (choice %in% seq_along(choices)) {
+      if (ind) {
+        return(choice)
+      }
 
-  choose_from_menu(
-    message = "Select an item from the menu ({.kbd {names(choices)}}),
-        or {.kbd {exit}} to exit.",
-    prompt = prompt,
-    choices,
-    exit
-  )
+      return(choices[[choice]])
+    }
+
+    choice <- cli_ask(
+      prompt = prompt,
+      .envir = .envir
+    )
+  }
 }
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 # @staticimports pkg:isstatic
-#  as_numbered_labels is_all_null
+# is_all_null
 
 # @staticimports pkg:staticimports
 #  map2
